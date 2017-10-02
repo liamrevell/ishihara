20c756e234e5c4a8ab92911fe045dfc16dab9d46
Liam J. Revell <liam.revell@umb.edu>
2017-10-02 17:09:59 -0500

    .onLoad added

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname){
+	cat("\n")
+	cat("   ***********************************************\n\n")
+	cat("   *    This package is just for fun & is not a  *\n")
+	cat("   *           medical diagnostic tool.          *\n\n")
+	cat("   ***********************************************\n\n")
+}
+
