4d8a0cad341fd18be08fef081e0f6670d83c9ddf
Liam J. Revell <liam.revell@umb.edu>
2017-10-03 09:03:53 -0500

    change .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname){
 	cat("\n")
-	cat("   ***********************************************\n")
-	cat("   *                                             *\n")
-	cat("   *    This package is just for fun & is not a  *\n")
-	cat("   *           medical diagnostic tool.          *\n")
-	cat("   *                                             *\n")
+	cat("   *************************************************\n")
+	cat("   *                                               *\n")
+	cat("   *    This package is just for fun & is not a    *\n")
+	cat("   *           medical diagnostic tool.            *\n")
+	cat("   *                                               *\n")
 	cat("   ***********************************************\n\n")
 }
 
